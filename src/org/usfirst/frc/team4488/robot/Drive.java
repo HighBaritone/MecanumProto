7f36afabb5cf2b15a90d1409c7d4700e1c4d0c55
Isaac Pavon <Isaac Pavon>
2017-01-12 19:40:00 -0800

    Making drive it's own class for more accurate control and monitoring

---
+++ src/org/usfirst/frc/team4488/robot/Drive.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team4488.robot;
+
+import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
+
+public class Drive {
+	
+	RobotDrive driveBase;
+	CANTalon FR;
+	CANTalon BR;
+	CANTalon FL;
+	CANTalon BL;
+	AHRS navx;
+	
+	public Drive(){
+		FR = new CANTalon(RobotMap.FrontRightMotor);
+		BR = new CANTalon(RobotMap.BackRightMotor);
+		FL = new CANTalon(RobotMap.FrontLeftMotor);
+		BL = new CANTalon(RobotMap.BackLeftMotor);
+        navx = new AHRS(SPI.Port.kMXP);
+        driveBase = new RobotDrive(FL, BL, FR, BR);
+	}
+	
+	public void teleopDrive(Joystick xbox){
+		driveBase.mecanumDrive_Cartesian(xbox.getRawAxis(1), xbox.getRawAxis(2), xbox.getRawAxis(4), navx.getYaw());
+	}
+
+}
