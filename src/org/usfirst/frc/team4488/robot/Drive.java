860159afbb5f662809bb4af38c8eb98493110f68
Isaac Pavon <Isaac Pavon>
2017-01-12 20:09:39 -0800

    Possible per-motor control method

--- src/org/usfirst/frc/team4488/robot/Drive.java
+++ src/org/usfirst/frc/team4488/robot/Drive.java
@@ -28,5 +28,14 @@ public class Drive {
 	public void teleopDrive(Joystick xbox){
 		driveBase.mecanumDrive_Cartesian(xbox.getRawAxis(1), xbox.getRawAxis(2), xbox.getRawAxis(4), navx.getYaw());
 	}
+	
+	
+	//Would this be a way to do autonomous driving?
+	public void setMotors(double FRpower, double BRpower, double FLpower, double BLpower){
+		FR.set(FRpower);
+		BR.set(BRpower);
+		FL.set(FLpower);
+		BL.set(BLpower);
+	}
 
 }
